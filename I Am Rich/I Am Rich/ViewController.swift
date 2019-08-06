d475588a6e70b2daf5c3d236d359ad6dbc252d2a
Kim Eklund <kim.eklund@moovel.com>
2019-08-05 21:41:23 -0700

    IAmRich.app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kim Eklund on 7/30/19.
+//  Copyright © 2019 Kim Eklund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
