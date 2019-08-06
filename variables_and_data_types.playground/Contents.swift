f1e07263bf3f9e74e2e48c036db2511fcd55e109
Kim Eklund <kim.eklund@moovel.com>
2019-08-05 21:42:29 -0700

    variables and data types playground

---
+++ variables_and_data_types.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge : Int = 21
+
+myAge = 33
+
+let myName : String = "Kim"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) Eklund"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 5
+let text : String = "abc"
+let boolType : Bool = true
+let floatType : Float = 1.3
+let doubleType : Double = 3.15468746168465
+
+
+
+
