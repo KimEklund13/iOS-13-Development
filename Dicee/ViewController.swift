ffb3df9fe418e4863d7892cef602de7fd08e9a7c
Kim Eklund <kim.eklund@moovel.com>
2019-08-05 21:44:07 -0700

    Dicee app

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Kim Eklund on 8/5/19.
+//  Copyright © 2019 Kim Eklund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDiceImages()
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        // generates random numbers for 6-1, so 0-5.
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+}
+
